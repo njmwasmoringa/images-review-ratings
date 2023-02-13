e99115bb1c0113b71bc5595567b68badb149c7fd
Julius <njmwas@gmail.com>
2023-02-13 20:11:47 +0300

    Finxed missing component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { ImagesProvider } from './context/images.provider';
 import { LoaderProvider } from './context/loader.provider';
-import { Images } from './components/images-grid/Images';
+import  Images  from './components/images-grid/Images';
 
 function App() {
   return (
     <LoaderProvider>
       <ImagesProvider>
-          <Images />
+        <Images />
       </ImagesProvider>
     </LoaderProvider>
   );
