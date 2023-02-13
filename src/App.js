d6081eca9ea6846848302f3769e3e055f74ca837
Julius <njmwas@gmail.com>
2023-02-13 19:57:06 +0300

    Added the image-grid component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { ImagesProvider } from './context/images.provider';
+import { LoaderProvider } from './context/loader.provider';
+import { Images } from './components/images-grid/Images';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoaderProvider>
+      <ImagesProvider>
+          <Images />
+      </ImagesProvider>
+    </LoaderProvider>
   );
 }
 
